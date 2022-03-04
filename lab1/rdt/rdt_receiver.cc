5ccf1c2f60097d55aff4295a2b4a66dffc8a9b46
Tommy Huang <13917078913@163.com>
2022-03-04 21:27:53 +0800

    update

--- lab1/rdt/rdt_receiver.cc
+++ lab1/rdt/rdt_receiver.cc
@@ -1,21 +1,15 @@
 /*
  * FILE: rdt_receiver.cc
  * DESCRIPTION: Reliable data transfer receiver.
- *              In this implementation, the packet format is laid out as the following:
+ * NOTE: In my implementation, the packet format is laid out as
+ *       the following:
  *
- *     1. The first packet of a message from sender to receiver
- *       |<-  2 byte  ->|<-  4 byte  ->|<-  1 byte  ->|<-  4 byte  ->|<-  the rest  ->|
- *       |   checksum   |  packet seq  | payload size | message size |     payload    |
+ *       |<-  2 bytes  ->|<-  4 bytes ->|<-             the rest            ->|
+ *       |   checksum    |  ack number  |              payload                |
  *
- *     2. The rest packets of a message from sender to receiver
- *       |<-  2 byte  ->|<-  4 byte  ->|<-  1 byte  ->|<-  the rest  ->|
- *       |   checksum   |  packet seq  | payload size |     payload    |
- *
- *     3. The ACK packet from receiver to sender
- *       |<-  2 byte  ->|<-  4 byte  ->|<-  the rest  ->|
- *       |   checksum   |    ack seq   |   meaningless  |
+ *       The first byte of each packet indicates the size of the payload
+ *       (excluding this single-byte header)
  */
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,22 +21,16 @@
 
 #define WINDOW_SIZE 10
 
-// 当前正在接受的message
-static message *current_message;
+static packet *packet_buffer;
 
-// 当前message已经构建完的byte数量
-static int message_cursor;
+bool valid[WINDOW_SIZE];
 
-// 接收方的数据包缓存
-static packet *receiver_buffer;
+static message *msg;
+static int cursor;
 
-// 数据包缓存的有效位
-static char *valid;
-
-// 应该收到的packet seq
 static int ack;
 
-const int header_size = 7; //checksum + payload_size + seq
+const int header_size = 7; //checksum + size + seq
 
 static short calc_checksum(struct packet *pkt) {
     long sum = 0;
@@ -55,29 +43,15 @@ static short calc_checksum(struct packet *pkt) {
     return ~sum;
 }
 
-void send_ack(int ack) {
-    packet pkt;          // ack包只包含checksum和ack即可
-    memcpy(pkt.data + 2, &ack, 4);
-    short checksum = calc_checksum(&pkt);
-    memcpy(pkt.data, &checksum, 2);
-    Receiver_ToLowerLayer(&pkt);
-}
-
-
 /* receiver initialization, called once at the very beginning */
 void Receiver_Init() {
     fprintf(stdout, "At %.2fs: receiver initializing ...\n", GetSimulationTime());
 
-    // 初始化current_message
-    current_message = (message *) malloc(sizeof(message));
-
-    // 初始化receiver_buffer和valid
-    receiver_buffer = (packet *) malloc(WINDOW_SIZE * sizeof(packet));
-    valid = (char *) malloc(WINDOW_SIZE);
-
-    // 其他初始化
+    msg = (message *) malloc(sizeof(message));
+    packet_buffer = (packet *) malloc(WINDOW_SIZE * sizeof(packet));
+    memset(valid,0,sizeof(valid));
     ack = 0;
-    message_cursor = 0;
+    cursor = 0;
 }
 
 /* receiver finalization, called once at the very end.
@@ -88,10 +62,19 @@ void Receiver_Final() {
     fprintf(stdout, "At %.2fs: receiver finalizing ...\n", GetSimulationTime());
 }
 
+void send_ack(int ack) {
+    packet pkt;          // ack包只包含checksum和ack即可
+    memcpy(pkt.data + 2, &ack, 4);
+    short checksum = calc_checksum(&pkt);
+    memcpy(pkt.data, &checksum, 2);
+    Receiver_ToLowerLayer(&pkt);
+}
+
+
 /* event handler, called when a packet is passed from the lower layer at the
    receiver */
 void Receiver_FromLowerLayer(struct packet *pkt) {
-    // 检查checksum，校验失败则直接抛弃
+    //检查checksum,若checksum不正确则丢包
     short checksum;
     memcpy(&checksum, pkt->data, 2);
     if (checksum != calc_checksum(pkt)) { // 校验失败
@@ -100,56 +83,51 @@ void Receiver_FromLowerLayer(struct packet *pkt) {
 
     int seq;
     memcpy(&seq, pkt->data + 2, 4);
-    if (ack < seq &&
-        seq < ack + WINDOW_SIZE) {
+    if (ack < seq && seq < ack + WINDOW_SIZE) {   //滑动窗口范围内
         int idx = seq % WINDOW_SIZE;
-        if (valid[idx] == 0) {
-            memcpy(receiver_buffer[idx].data, pkt->data, RDT_PKTSIZE);
-            valid[idx] = 1;
+        if (!valid[idx]) {
+            memcpy(packet_buffer[idx].data, pkt->data, RDT_PKTSIZE);
+            valid[idx] = true;
         }
         send_ack(ack - 1);
         return;
     } else if (seq != ack) {
         send_ack(ack - 1);
-        return;
-    } else if (seq == ack) { // 收到了想要的数据包
-        int payload_size;
-        while (1) {
-            ++ack;
-            // memcpy(&payload_size, pkt->data + 2 + 4, sizeof(char));
-            // WAERNING: 如果用上面的方法给payload_size赋值会出错！
-            payload_size = pkt->data[header_size - 1];
+    }
 
-            // 判断是不是第一个包，并将payload写入current_message
-            if (message_cursor == 0) {
-                if (current_message->size != 0) {
-                    current_message->size = 0;
-                    free(current_message->data);
-                }
-                payload_size -= 4; // 减去message_size占用的4个byte
-                memcpy(&(current_message->size), pkt->data + header_size, 4);
-                current_message->data = (char *) malloc(current_message->size);
-                memcpy(current_message->data + message_cursor, pkt->data + header_size + 4, payload_size);
-                message_cursor += payload_size;
+    if (seq == ack) { //seq=ack, 考虑buffer中的包
+        int size;
+        while (true) {
+            ack++;
+            size = (int) pkt->data[header_size - 1];
+
+            if (cursor == 0) {  //第一个包有message size, +4
+                size -= 4;
+//                std::cout<<"checkpoint 1";
+                memcpy(&msg->size, pkt->data + header_size, 4);
+                msg->data = (char *) malloc(msg->size);
+//                std::cout<<"checkpoint 2";
+                memcpy(msg->data + cursor, pkt->data + header_size + 4, size);
+                cursor += size;
             } else {
-                memcpy(current_message->data + message_cursor, pkt->data + header_size, payload_size);
-                message_cursor += payload_size;
+                memcpy(msg->data + cursor, pkt->data + header_size, size);
+                cursor += size;
             }
 
-            // 检查current_message是否构建完成
-            if (message_cursor == current_message->size) {
-                Receiver_ToUpperLayer(current_message);
-                message_cursor = 0;
+            if (cursor == msg->size) {      //整个message组装好发送
+                Receiver_ToUpperLayer(msg);
+                cursor = 0;
             }
 
             int idx = ack % WINDOW_SIZE;
             if (valid[idx] == 1) {
-                pkt = &receiver_buffer[idx];
+//                std::cout<<"valid=1, idx="<<idx<<std::endl;
+                pkt = &packet_buffer[idx];
                 memcpy(&seq, pkt->data + 2, 4);
                 valid[idx] = 0;
             } else {
                 send_ack(seq);
-                return;
+                break;
             }
         }
     }
--- lab1/rdt/rdt_sender.cc
+++ lab1/rdt/rdt_sender.cc
@@ -1,19 +1,14 @@
 /*
  * FILE: rdt_sender.cc
  * DESCRIPTION: Reliable data transfer sender.
- *              In this implementation, the packet format is laid out as the following:
+ * NOTE:  In my implementation, the packet format is laid out as
+ *       the following:
  *
- *     1. The first packet of a message from sender to receiver
- *       |<-  2 byte  ->|<-  4 byte  ->|<-  1 byte  ->|<-  4 byte  ->|<-  the rest  ->|
- *       |   checksum   |  packet seq  | payload size | message size |     payload    |
+ *       |<-  2 bytes  ->|<-  4 bytes ->|<-  1 byte ->|<-             the rest            ->|
+ *       |   checksum   |  seq number  | payload size |            payload                 |
  *
- *     2. The rest packets of a message from sender to receiver
- *       |<-  2 byte  ->|<-  4 byte  ->|<-  1 byte  ->|<-  the rest  ->|
- *       |   checksum   |  packet seq  | payload size |     payload    |
- *
- *     3. The ACK packet from receiver to sender
- *       |<-  2 byte  ->|<-  4 byte  ->|<-  the rest  ->|
- *       |   checksum   |    ack seq   |   meaningless  |
+ *       The first byte of each packet indicates the size of the payload
+ *       (excluding this single-byte header)
  */
 
 
@@ -21,6 +16,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
+#include <queue>
 
 #include "rdt_struct.h"
 #include "rdt_sender.h"
@@ -45,19 +41,20 @@ static int next_message_seq;
 static int cursor;
 
 // 存储待发送的数据包，大小为WINDOW_SIZE
-static packet* packet_window;
+static packet* window;
 
-// packet_window中的包的数量
-static int num_packet;
 
-// 下一个进入packet_window的数据包的序号
-static int next_packet_seq;
+// window中的包的数量
+static int now_size; // now_size维护目前buffer的大小
+
+// 下一个进入window的数据包的序号
+static int mx_seq;
 
 // 下一个要被发送的数据包序号
-static int next_packet_to_send_seq;
+static int next_seq;
 
 // 应该接收到的ack序号
-static int expected_ack_seq;
+static int mx_ack;
 
 const int header_size=7;
 
@@ -72,44 +69,77 @@ static short calc_checksum(struct packet *pkt) {
     return ~sum;
 }
 
+/* sender initialization, called once at the very beginning */
+void Sender_Init() {
+    fprintf(stdout, "At %.2fs: sender initializing ...\n", GetSimulationTime());
+    message_buffer = (message *)malloc((MAX_BUFFER_SIZE) * sizeof(message));
+    memset(message_buffer, 0, (MAX_BUFFER_SIZE) * sizeof(message));
+    num_message = 0;
+    current_message_seq = 0;
+    next_message_seq = 0;
+    cursor = 0;
+
+    window = (packet *)malloc(WINDOW_SIZE * sizeof(packet));
+    memset(window, 0, WINDOW_SIZE * sizeof(packet));
+    now_size = 0;
+    mx_seq = 0;
+    next_seq = 0;
+    mx_ack = 0;
+}
+
+/* sender finalization, called once at the very end.
+   you may find that you don't need it, in which case you can leave it blank.
+   in certain cases, you might want to take this opportunity to release some
+   memory you allocated in Sender_init(). */
+void Sender_Final() {
+    fprintf(stdout, "At %.2fs: sender finalizing ...\n", GetSimulationTime());
+    for (int i = 0; i < MAX_BUFFER_SIZE; ++i) {
+        if (message_buffer[i].size > 0) {
+            free(message_buffer[i].data);
+        }
+    }
+    free(message_buffer);
+    free(window);
+}
+
 void Chunk_Message() {
     int current_message_index = current_message_seq % MAX_BUFFER_SIZE;
     message msg = message_buffer[current_message_index];
     packet pkt;
     short checksum;
 
-    // 每次拆出一个数据包并存入packct_window，直到packet_window满了，或message_buffer中不再有待拆分的消息
-    while (num_packet < WINDOW_SIZE && current_message_seq < next_message_seq) {
+    // 每次拆出一个数据包并存入packct_window，直到window满了，或message_buffer中不再有待拆分的消息
+    while (now_size < WINDOW_SIZE && current_message_seq < next_message_seq) {
         // 若剩余待拆分的byte数量太多，不能装入一个packet的payload
         if (msg.size - cursor > RDT_PKTSIZE - header_size) {
             // 写包头和数据
-            memcpy(pkt.data + 2, &next_packet_seq, 4);
+            memcpy(pkt.data + 2, &mx_seq, 4);
             pkt.data[header_size - 1] = RDT_PKTSIZE - header_size;
             memcpy(pkt.data + header_size, msg.data + cursor, RDT_PKTSIZE - header_size);
             // 计算checksum
             checksum = calc_checksum(&pkt);
             memcpy(pkt.data, &checksum, 2);
-            // 将pkt的内容存入packet_window，位置为next_packet_index
-            int next_packet_index = next_packet_seq % WINDOW_SIZE;
-            memcpy(packet_window[next_packet_index].data, &pkt, sizeof(packet));
+            // 将pkt的内容存入window，位置为next_packet_index
+            int next_packet_index = mx_seq % WINDOW_SIZE;
+            memcpy(window[next_packet_index].data, &pkt, sizeof(packet));
             cursor += RDT_PKTSIZE - header_size;
-            ++next_packet_seq;
-            ++num_packet;
+            ++mx_seq;
+            ++now_size;
         }
             // 剩余待拆分的byte可以一次性装入一个packet
         else if (cursor < msg.size) {
             // 写包头和数据
-            memcpy(pkt.data + 2, &next_packet_seq, 4);
+            memcpy(pkt.data + 2, &mx_seq, 4);
             pkt.data[header_size - 1] = msg.size - cursor;
             memcpy(pkt.data + header_size, msg.data + cursor, msg.size - cursor);
             // 计算checksum
             checksum = calc_checksum(&pkt);
             memcpy(pkt.data, &checksum, 2);
-            // 将pkt的内容存入packet_window，位置为next_packet_index
-            int next_packet_index = next_packet_seq % WINDOW_SIZE;
-            memcpy(packet_window[next_packet_index].data, &pkt, sizeof(packet));
-            ++next_packet_seq;
-            ++num_packet;
+            // 将pkt的内容存入window，位置为next_packet_index
+            int next_packet_index = mx_seq % WINDOW_SIZE;
+            memcpy(window[next_packet_index].data, &pkt, sizeof(packet));
+            ++mx_seq;
+            ++now_size;
             // 当前消息拆分完毕，将其从message_buffer中移除
             ++current_message_seq;
             cursor = 0;
@@ -131,47 +161,12 @@ void Chunk_Message() {
 
 void Send_Packet() {
     packet pkt;
-    // 每次发一个包，直到packet_window中的包全部发完
-    while (next_packet_to_send_seq < next_packet_seq) {
-        memcpy(&pkt, &(packet_window[next_packet_to_send_seq % WINDOW_SIZE]), sizeof(packet));
+    // 每次发一个包，直到window中的包全部发完
+    while (next_seq < mx_seq) {
+        memcpy(&pkt, &(window[next_seq % WINDOW_SIZE]), sizeof(packet));
         Sender_ToLowerLayer(&pkt);
-        ++next_packet_to_send_seq;
+        ++next_seq;
     }
-}
-
-/* sender initialization, called once at the very beginning */
-void Sender_Init() {
-    fprintf(stdout, "At %.2fs: sender initializing ...\n", GetSimulationTime());
-    // 初始化message_buffer
-    message_buffer = (message *)malloc((MAX_BUFFER_SIZE) * sizeof(message));
-    memset(message_buffer, 0, (MAX_BUFFER_SIZE) * sizeof(message));
-    num_message = 0;
-    current_message_seq = 0;
-    next_message_seq = 0;
-    cursor = 0;
-
-    // 初始化pakcet_window
-    packet_window = (packet *)malloc(WINDOW_SIZE * sizeof(packet));
-    memset(packet_window, 0, WINDOW_SIZE * sizeof(packet));
-    num_packet = 0;
-    next_packet_seq = 0;
-    next_packet_to_send_seq = 0;
-    expected_ack_seq = 0;
-}
-
-/* sender finalization, called once at the very end.
-   you may find that you don't need it, in which case you can leave it blank.
-   in certain cases, you might want to take this opportunity to release some
-   memory you allocated in Sender_init(). */
-void Sender_Final() {
-    fprintf(stdout, "At %.2fs: sender finalizing ...\n", GetSimulationTime());
-    for (int i = 0; i < MAX_BUFFER_SIZE; ++i) {
-        if (message_buffer[i].size > 0) {
-            free(message_buffer[i].data);
-        }
-    }
-    free(message_buffer);
-    free(packet_window);
 }
 
 /* event handler, called when a message is passed from the upper layer at the
@@ -220,16 +215,16 @@ void Sender_FromLowerLayer(struct packet *pkt) {
     int ack_seq;
     memcpy(&ack_seq, pkt->data + 2, 4);
     // 若收到的ack_seq在正常范围，则说明小于ack的packet都已经被receiver正常接收
-    if (expected_ack_seq <= ack_seq && ack_seq < next_packet_seq) {
-        num_packet -= (ack_seq - expected_ack_seq + 1); // 确认接收的包均从packet_window中可移除
-        expected_ack_seq = ack_seq + 1;
+    if (mx_ack <= ack_seq && ack_seq < mx_seq) {
+        now_size -= (ack_seq - mx_ack + 1); // 确认接收的包均从window中可移除
+        mx_ack = ack_seq + 1;
         // 开始新一轮发包
         Sender_StartTimer(TIMEOUT); // 若上一轮计时没有结束，则该命令重置计时器
         Chunk_Message();
         Send_Packet();
     }
-    // 若packet_window中所有的包都已确认被receiver接收，则结束计时器
-    if (ack_seq == next_packet_seq - 1) {
+    // 若window中所有的包都已确认被receiver接收，则结束计时器
+    if (ack_seq == mx_seq - 1) {
         Sender_StopTimer();
     }
 }
@@ -237,7 +232,7 @@ void Sender_FromLowerLayer(struct packet *pkt) {
 /* event handler, called when the timer expires */
 void Sender_Timeout() {
     // 超时，该收到的ack包没收到，重发
-    next_packet_to_send_seq = expected_ack_seq;
+    next_seq = mx_ack;
     Sender_StartTimer(TIMEOUT);
     Chunk_Message();
     Send_Packet();
